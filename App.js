945ae0fdce44a4892003cb037a569dd61e56d9de
haoniu08 <niu.hao@northeastern.edu>
2024-09-12 12:39:43 -0700

    added TextInput component, not yet componetized

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import Header from './comonents/Header';
+import { useState } from 'react';
 
 export default function App() {
 
   const appName = "WaWaWeeWa";
+  const [text, setText] = useState("");
+
 
   return (
     <View style={styles.container}>      
-      <Header name={appName} >
-        <Text>Header Children</Text>
-        </Header>
+      <Header name={appName} />
+      <TextInput 
+        placeholder='Please enter the text here' 
+        keyboardType='default' 
+        style={{borderBottomColor: "purple", borderBottomWidth: 2}}
+        value={text}
+        onChangeText={function (ChangeText) {
+          setText(ChangeText);
+        }}
+      />
+      <Text>{text}</Text>
       <StatusBar style="auto" />
     </View>
   );
