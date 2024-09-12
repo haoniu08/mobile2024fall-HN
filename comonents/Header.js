61c188a19899541e02c0d3bcef05ceb8d24e9931
haoniu08 <niu.hao@northeastern.edu>
2024-09-12 12:04:11 -0700

    double-checking commits on destructure & props

--- comonents/Header.js
+++ comonents/Header.js
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-// prop needs to be received here
+// prop needs to be received here (destructured)
 export default function Header({ name }) {
   return (
     <View>
