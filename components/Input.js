f0fbbc449bd0a58e396b5532c21806351a67f693
haoniu08 <niu.hao@northeastern.edu>
2024-09-19 10:42:21 -0700

    modal styling added, wrapped container with new view

--- components/Input.js
+++ components/Input.js
@@ -1,59 +1,49 @@
-// Import button here
-import { Button, Modal, StyleSheet, TextInput, Text, View } from 'react-native'
-import React, {useState} from 'react'
+import { Button, Modal, StyleSheet, TextInput, Text, View } from 'react-native';
+import React, { useState } from 'react';
 
-// add inputHandler here
-export default function Input( {
-  autoFocus,
-  inputHandler, 
-  isModalVisible,
-} ) {
+export default function Input({ autoFocus, inputHandler, isModalVisible }) {
   const [text, setText] = useState("");
   const [blur, setBlur] = useState(false);
 
-  // The function logs what user has typed in the TextInput to console 
   function handleConfirm() {
-    // add inputHandler here
     inputHandler(text);
   }
 
   return (
-    // just wrapping everything in a Modal
-    <Modal animationType="slide" visible={isModalVisible}>
+    <Modal 
+      animationType="slide" 
+      transparent={true}
+      visible={isModalVisible}>
       <View style={styles.container}>
-        <TextInput 
-          placeholder='Please enter the text here' 
-          keyboardType='default' 
-          style={{borderBottomColor: "purple", borderBottomWidth: 2}}
-          value={text}
-          autoFocus={autoFocus}
-          // onChangeText={updateText}, removed
-          onChangeText={ (changedText) => {
-            setText(changedText);
-          }}
-          // onBlur={handleBlur}, original function removed
-          onBlur={ () => {
-            setBlur(true);
-          }}
-          // This makes the character count appear again when user starts to type
-          onFocus={ () => {
-            setBlur(false);
-          }}
-        />
-        {/* The code (Neda) replaces the original function - handle blur etc. */}
-        {blur ? (
-          text.length >= 3 ? (
-            <Text>Thank you</Text>
+        <View style={styles.innerContainer}>
+          <TextInput 
+            placeholder='Please enter the text here' 
+            keyboardType='default' 
+            style={{borderBottomColor: "purple", borderBottomWidth: 2}}
+            value={text}
+            autoFocus={autoFocus}
+            onChangeText={(changedText) => {
+              setText(changedText);
+            }}
+            onBlur={() => {
+              setBlur(true);
+            }}
+            onFocus={() => {
+              setBlur(false);
+            }}
+          />
+          {blur ? (
+            text.length >= 3 ? (
+              <Text>Thank you</Text>
+            ) : (
+              <Text>Please type more than 3 characters</Text>
+            )
           ) : (
-            <Text>Please type more than 3 characters</Text>
-          )
-        ) : (
-          text && <Text>{text.length}</Text>
-        )}
-        {/* The confirm button & onPress prop (event handler) */}
-        {/* onPress accepts a function as its value */}
-        <View style={styles.buttonContainer}>
-          <Button title="Confirm" onPress={handleConfirm} />
+            text && <Text>{text.length}</Text>
+          )}
+          <View style={styles.buttonContainer}>
+            <Button title="Confirm" onPress={handleConfirm} />
+          </View>
         </View>
       </View>
     </Modal>   
@@ -67,7 +57,13 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  innerContainer: {
+    backgroundColor: 'lightblue',
+    padding: 20,
+    borderRadius: 10,
     alignItems: 'center',
     justifyContent: 'center',
   },
