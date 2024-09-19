448b5eed6d31bcd1230fce4af9bef0577022e98f
haoniu08 <niu.hao@northeastern.edu>
2024-09-18 21:39:35 -0700

    trying out Neda's code for w2

--- components/Input.js
+++ components/Input.js
@@ -1,28 +1,11 @@
 import { StyleSheet, TextInput, Text, View } from 'react-native'
 import React, {useState} from 'react'
 
-export default function Input( {autoFocus = false} ) {
+export default function Input( {autoFocus} ) {
 
   const [text, setText] = useState("");
-  const [showCharCount, setShowCharCount] = useState(false);
-  const [message, setMessage] = useState("");
+  const [blur, setBlur] = useState(false);
 
-  function updateText(newText) {
-    setText(newText);
-    setShowCharCount(true);
-    setMessage("");
-  }
-
-  function handleBlur() {
-    setShowCharCount(false);
-    // show messages here checking if >= or < 3 characters
-    if (text.length >= 3) {
-        setMessage("Thank you")
-    } else {
-        setMessage("Please type more than 3 characters")
-    }
-  }
-    
   return (
     <View>
       <TextInput 
@@ -30,22 +13,31 @@ export default function Input( {autoFocus = false} ) {
         keyboardType='default' 
         style={{borderBottomColor: "purple", borderBottomWidth: 2}}
         value={text}
-        onChangeText={updateText}
-        onBlur={handleBlur}
         autoFocus={autoFocus}
+        // onChangeText={updateText}
+        onChangeText={ (changedText) => {
+          setText(changedText);
+        }}
+        // onBlur={handleBlur}
+        onBlur={ () => {
+          setBlur(true);
+        }}
+        onFocus={ () => {
+          setBlur(false);
+        }}
       />
-      {showCharCount && text.length > 0 && (
-        <Text style={styles.charCount}>
-            {text.length} characters
-        </Text>
-      )}
-      {message.length > 0 && (
-        <Text style={styles.message}>
-            {message}
-        </Text>
+
+      {blur ? (
+        text.length >= 3 ? (
+          <Text>Thank you</Text>
+        ) : (
+          <Text>Please type more than 3 characters</Text>
+        )
+      ) : (
+        text && <Text>{text.length}</Text>
       )}
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({})
