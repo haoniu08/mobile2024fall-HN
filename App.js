23cb7896d7cb4f13528d34a373baa62dae5ab1a9
haoniu08 <niu.hao@northeastern.edu>
2024-09-18 23:15:07 -0700

    Goal button, modal component added, code works, yet to be able to handle input

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import {useState} from 'react';
 import Header from './components/Header';
 import Input from './components/Input';
@@ -9,18 +9,27 @@ export default function App() {
   const appName = "Don Baguette";
   // define a state variable to store the received data
   const [receivedData, setReceivedData] = useState("");
+  // set the initial state of the modal to false
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   // define a function to handle the received data
   function handleReceivedData(data) {
     // console.log("App.js", data);, still prints out at the terminal
     setReceivedData(data);
+    setIsModalVisible(false);
   }
 
   return (
     <View style={styles.container}>      
       <StatusBar style="auto" />
       <Header name={appName} /> 
-      <Input autoFocus={true} inputHandler={handleReceivedData}/>
+      {/* <Input autoFocus={true} inputHandler={handleReceivedData}/> */}
+      <Button 
+        title="Add a Goal" 
+        onPress={function () {
+          setIsModalVisible(true);
+        }} 
+      />
       <Text>{receivedData}</Text>
     </View>
   );
--- components/Input.js
+++ components/Input.js
@@ -1,10 +1,13 @@
 // Import button here
-import { Button, StyleSheet, TextInput, Text, View } from 'react-native'
+import { Button, Modal, StyleSheet, TextInput, Text, View } from 'react-native'
 import React, {useState} from 'react'
 
 // add inputHandler here
-export default function Input( {autoFocus, inputHandler} ) {
-
+export default function Input( {
+  autoFocus,
+  inputHandler, 
+  isModalVisible,
+} ) {
   const [text, setText] = useState("");
   const [blur, setBlur] = useState(false);
 
@@ -15,41 +18,51 @@ export default function Input( {autoFocus, inputHandler} ) {
   }
 
   return (
-    <View>
-      <TextInput 
-        placeholder='Please enter the text here' 
-        keyboardType='default' 
-        style={{borderBottomColor: "purple", borderBottomWidth: 2}}
-        value={text}
-        autoFocus={autoFocus}
-        // onChangeText={updateText}, removed
-        onChangeText={ (changedText) => {
-          setText(changedText);
-        }}
-        // onBlur={handleBlur}, original function removed
-        onBlur={ () => {
-          setBlur(true);
-        }}
-        // This makes the character count appear again when user starts to type
-        onFocus={ () => {
-          setBlur(false);
-        }}
-      />
-      {/* The code (Neda) replaces the original function - handle blur etc. */}
-      {blur ? (
-        text.length >= 3 ? (
-          <Text>Thank you</Text>
+    // just wrapping everything in a Modal
+    <Modal animationType="slide" visible={isModalVisible}>
+      <View style={styles.container}>
+        <TextInput 
+          placeholder='Please enter the text here' 
+          keyboardType='default' 
+          style={{borderBottomColor: "purple", borderBottomWidth: 2}}
+          value={text}
+          autoFocus={autoFocus}
+          // onChangeText={updateText}, removed
+          onChangeText={ (changedText) => {
+            setText(changedText);
+          }}
+          // onBlur={handleBlur}, original function removed
+          onBlur={ () => {
+            setBlur(true);
+          }}
+          // This makes the character count appear again when user starts to type
+          onFocus={ () => {
+            setBlur(false);
+          }}
+        />
+        {/* The code (Neda) replaces the original function - handle blur etc. */}
+        {blur ? (
+          text.length >= 3 ? (
+            <Text>Thank you</Text>
+          ) : (
+            <Text>Please type more than 3 characters</Text>
+          )
         ) : (
-          <Text>Please type more than 3 characters</Text>
-        )
-      ) : (
-        text && <Text>{text.length}</Text>
-      )}
-      {/* The confirm button & onPress prop (event handler) */}
-      {/* onPress accepts a function as its value */}
-      <Button title="Confirm" onPress={handleConfirm} />
-    </View>
+          text && <Text>{text.length}</Text>
+        )}
+        {/* The confirm button & onPress prop (event handler) */}
+        {/* onPress accepts a function as its value */}
+        <Button title="Confirm" onPress={handleConfirm} />
+      </View>
+    </Modal>   
   );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
