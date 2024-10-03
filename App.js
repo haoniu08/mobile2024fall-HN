d0ef6fdbb702215b26b7534b930dc6f53cd1a42e
haoniu08 <niu.hao@northeastern.edu>
2024-10-02 21:28:04 -0700

    app.js updated, navigationContainer imported and applied to Home

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Home from './components/Home'
+import { NavigationContainer } from '@react-navigation/native'
 
 export default function App() {
-  return <Home />
+  return (
+    <NavigationContainer>
+      <Home />
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({})
