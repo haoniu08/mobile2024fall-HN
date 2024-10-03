0d9d02647a014cbb1c84b803b10268e9a6af8aa0
haoniu08 <niu.hao@northeastern.edu>
2024-10-02 21:40:33 -0700

    Stack - createStackNavigator created

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Home from './components/Home'
 import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
