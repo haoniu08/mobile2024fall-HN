22da1df892387713164059021c33b981ee794d88
haoniu08 <niu.hao@northeastern.edu>
2024-10-02 23:09:08 -0700

    option prop passed to to stack.screen - "home", updated title, background color, and font color

--- App.js
+++ App.js
@@ -11,7 +11,15 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen 
+          name="Home" 
+          component={Home} 
+          options={{
+            headerStyle: {backgroundColor: "violet"},
+            headerTintColor: "white",
+            title : "My Goals",
+          }}
+        />
         <Stack.Screen name="Details" component={GoalDetails} />
       </ Stack.Navigator>
     </NavigationContainer>
