c011ef7fd8fd653dab4b93d48772078600105a17
haoniu08 <niu.hao@northeastern.edu>
2024-11-14 12:22:27 -0800

    Added static map to profile page

--- components/LocationManager.js
+++ components/LocationManager.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Button, Alert } from 'react-native'
+import { StyleSheet, Text, View, Button, Alert, Image } from 'react-native'
 import React, {useState} from 'react'
 import * as Location from 'expo-location';
 
@@ -35,17 +35,35 @@ export default function LocationManager() {
             setLocation({
                 latitude: locationResponse.coords.latitude,
                 longitude: locationResponse.coords.longitude
-            });            
+            });
+            if(location) {
+                console.log(location);
+            }           
         } catch (error) {
             console.log("Error getting location", error);
         }
       }
 
+      if (location) {
+        console.log(`https://maps.googleapis.com/maps/api/staticmap?center=${location.latitude},${location.longitude}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:L%7C${location.latitude},${location.longitude}&key=${process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY}`);
+      }
+
   return (
     <View>
       <Button title="Locate User" onPress={handleLocateUser}/>
+      { 
+        location && 
+        <Image 
+            style={styles.image}
+            source={{uri: `https://maps.googleapis.com/maps/api/staticmap?center=${location.latitude},${location.longitude}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:L%7C${location.latitude},${location.longitude}&key=${process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY}`}} />
+      }
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    image:{
+        width: window.width,
+        height: 200
+    }
+})
