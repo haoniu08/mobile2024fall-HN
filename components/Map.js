bafaf7e84d1d7155cf6e862134efa1c3c0dfe7cf
haoniu08 <niu.hao@northeastern.edu>
2024-11-14 12:45:15 -0800

    Updated Marker in the dynamic map, so market shows when user clicks on the map

--- components/Map.js
+++ components/Map.js
@@ -1,22 +1,37 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import MapView from "react-native-maps";
+import React, {useState} from 'react'
+import MapView, { Marker } from "react-native-maps";
 
 export default function Map() {
+
+    const[selectedLocation, setSelectedLocation] = useState(null);
+
   return (
     <MapView
+        style={styles.map}
         intialRegion={{
             latitude: 37.78825,
             longitude: -122.4324,
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
         }}
-        style={{width: '100%', height: '40%'}}
-    />
+        
+        onPress={ (e) => {
+            setSelectedLocation({
+                latitude: e.nativeEvent.coordinate.latitude,
+                longitude: e.nativeEvent.coordinate.longitude,
+            })
+        }}
+    >
+        <Marker coordinate={selectedLocation}/>
+    </MapView>
   )
 }
 
 const styles = StyleSheet.create({
-
+    map: {
+        width: "100%",
+        height: "50%",
+    }
 })
 
